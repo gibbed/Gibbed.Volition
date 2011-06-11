1fed09fc5e7aa9c29cedfc93423380399ba63b84
Rick Gibbed <gibbed@users.noreply.github.com>
2011-06-11 16:31:55 +0000

    - Fix a few visual issues in Unpack. - PegFile deserialize implementation.

---
+++ Gibbed.Volition.FileFormats/Peg/PixelFormat.cs
@@ -0,0 +1,76 @@
+﻿/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+namespace Gibbed.Volition.FileFormats.Peg
+{
+    public enum PixelFormat : ushort
+    {
+        // Supported by: SR2, RFG, RFA
+        //    DX9: D3DFMT_DXT1
+        //   DX10: DXGI_FORMAT_BC1_TYPELESS
+        DXT1 = 400,
+        
+        // Supported by: SR2, RFG, RFA
+        //    DX9: D3DFMT_DXT3 (DX9)
+        //   DX10: DXGI_FORMAT_BC2_TYPELESS
+        DXT3 = 401,
+        
+        // Supported by: SR2, RFG, RFA
+        //    DX9: D3DFMT_DXT5
+        //   DX10: DXGI_FORMAT_BC3_TYPELESS
+        DXT5 = 402,
+
+        // Supported by: SR2, RFG, RFA
+        //    DX9: D3DFMT_R5G6B5
+        //   DX10: DXGI_FORMAT_B5G6R5_UNORM
+        R5G6B5 = 403,
+
+        // Supported by: SR2, RFA (DX9)
+        //    DX9: D3DFMT_A1R5G5B5
+        A1R5G5B5 = 404,
+
+        // Supported by: SR2, RFA (DX9)
+        //    DX9: D3DFMT_A4R4G4B4
+        A4R4G4B4 = 405,
+
+        // Supported by: SR2, RFA (DX9)
+        //    DX9: D3DFMT_R8G8B8
+        R8G8B8 = 406,
+
+        // Supported by: SR2, RFG, RFA
+        //    DX9: D3DFMT_A8R8G8B8
+        //   DX10: DXGI_FORMAT_B8G8R8A8_TYPELESS
+        A8R8G8B8 = 407,
+
+        // Supported by: SR2, RFG, RFA (DX9)
+        //    DX9: D3DFMT_V8U8
+        V8U8 = 408,
+
+        // Supported by: SR2, RFA (DX9)
+        //    DX9: D3DFMT_CxV8U8
+        CxV8U8 = 409,
+
+        // Supported by: SR2
+        //    DX9: D3DFMT_A8
+        A8 = 410,
+    }
+}
--- Gibbed.Volition.FileFormats/PegFile.cs
+++ Gibbed.Volition.FileFormats/PegFile.cs
@@ -22,17 +22,25 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
+using System.Text;
 using Gibbed.Helpers;
 
 namespace Gibbed.Volition.FileFormats
 {
     public class PegFile
     {
+        public bool LittleEndian;
+        public ushort Version;
+        public Platform Platform;
+        public uint DataSize;
+        public List<Peg.Texture> Textures
+            = new List<Peg.Texture>();
+
         public void Deserialize(Stream input)
         {
+            var baseOffset = input.Position;
+
             var magic = input.ReadValueU32(true);
             if (magic != 0x564B4547 && // VKEG
                 magic != 0x47454B56)
@@ -41,23 +49,105 @@ namespace Gibbed.Volition.FileFormats
             }
 
             var littleEndian = magic == 0x564B4547;
+            this.LittleEndian = littleEndian;
 
-            var majorVersion = input.ReadValueU16(littleEndian);
-            if (majorVersion != 10)
+            var version = input.ReadValueU16(littleEndian);
+            if (version != 10)
             {
                 throw new FormatException("unsupported peg version");
             }
+            this.Version = version;
 
-            var minorVersion = input.ReadValueU16(littleEndian);
+            var platform = input.ReadValueU16(littleEndian);
+            if (Enum.IsDefined(typeof(Platform), platform) == false)
+            {
+                throw new FormatException("unsupported peg platform");
+            }
+            this.Platform = (Platform)platform;
 
             var headerSize = input.ReadValueU32(littleEndian);
-            if (input.Position + headerSize > input.Length)
+            if (baseOffset + headerSize > input.Length)
             {
                 throw new EndOfStreamException();
             }
 
-            var dataSize = input.ReadValueU32(littleEndian);
+            this.DataSize = input.ReadValueU32(littleEndian);
 
+            var textureCount = input.ReadValueU16();
+            var unknown12 = input.ReadValueU16();
+            var frameCount = input.ReadValueU16();
+            var unknown16 = input.ReadValueU8();
+            var unknown17 = input.ReadValueU8();
+
+            if (unknown12 != 0 ||
+                unknown16 != 16 ||
+                unknown17 != 0)
+            {
+                throw new FormatException("unexpected unknown values");
+            }
+
+            var textures = new Peg.Texture[textureCount];
+            int totalFrames = 0;
+            for (int i = 0; i < textures.Length; i++)
+            {
+                textures[i] = new Peg.Texture();
+
+                var frame = new Peg.Frame();
+                frame.Deserialize(input, littleEndian);
+                textures[i].Frames.Add(frame);
+                totalFrames++;
+
+                /* some pegs in SR2 seem to have been saved incorrectly?
+                 * or there is some sort of flag to mark that data is in
+                 * the same file as the header
+                 * 
+                 * see:
+                 *   ige_regular.peg_pc
+                 *   ige_small.peg_pc
+                 *   ige_smallest.peg_pc */
+                /*
+                if (i == 0 &&
+                    frame.DataOffset != 0)
+                {
+                    throw new FormatException("strange data offset");
+                }
+                */
+
+                if (frame.FrameCount == 0)
+                {
+                    throw new FormatException("frame count is 0");
+                }
+
+                for (int j = 1; j < frame.FrameCount; j++)
+                {
+                    var extraFrame = new Peg.Frame();
+                    extraFrame.Deserialize(input, littleEndian);
+
+                    if (extraFrame.FrameCount != 1)
+                    {
+                        throw new FormatException("frame count is not 1");
+                    }
+
+                    textures[i].Frames.Add(extraFrame);
+                    totalFrames++;
+                }
+            }
+
+            if (totalFrames != frameCount)
+            {
+                throw new FormatException("did not read correct amount of frames");
+            }
+
+            for (int i = 0; i < textures.Length; i++)
+            {
+                textures[i].Name = input.ReadStringZ(Encoding.ASCII);
+            }
+
+            this.Textures.AddRange(textures);
+        }
+
+        public void Serialize(Stream output)
+        {
             throw new NotImplementedException();
         }
     }
