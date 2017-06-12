0c0256cc851be320ea46b2b826f6a9ca225ae850
gibbed <rick@gibbed.us>
2017-06-12 04:28:36 -0500

    AmbientSoundElement improvements.

--- projects/Gibbed.RedFaction2.FileFormats/Level/Data/AmbientSoundElement.cs
+++ projects/Gibbed.RedFaction2.FileFormats/Level/Data/AmbientSoundElement.cs
@@ -20,34 +20,111 @@
  *    distribution.
  */
 
-using System;
 using System.IO;
 using System.Text;
 using Gibbed.IO;
+using Newtonsoft.Json;
 
 namespace Gibbed.RedFaction2.FileFormats.Level.Data
 {
+    [JsonObject(MemberSerialization.OptIn)]
     public class AmbientSoundElement : ISerializableElement
     {
-        public class ArrayElement : SerializableArrayElement<AmbientSoundElement>
+        #region Fields
+        private uint _Uid;
+        private Vector3 _Position;
+        private bool _PlayInEditor;
+        private string _SoundFileName;
+        private float _MinimumDistance;
+        private float _VolumeScale;
+        private float _Rolloff;
+        private uint _StartDelay;
+        #endregion
+
+        #region Properties
+        [JsonProperty("uid")]
+        public uint Uid
         {
+            get { return this._Uid; }
+            set { this._Uid = value; }
         }
+
+        [JsonProperty("pos")]
+        public Vector3 Position
+        {
+            get { return this._Position; }
+            set { this._Position = value; }
+        }
+
+        [JsonProperty("play_in_editor")]
+        public bool PlayInEditor
+        {
+            get { return this._PlayInEditor; }
+            set { this._PlayInEditor = value; }
+        }
+
+        [JsonProperty("sound_file")]
+        public string SoundFileName
+        {
+            get { return this._SoundFileName; }
+            set { this._SoundFileName = value; }
+        }
+
+        [JsonProperty("min_dist")]
+        public float MinimumDistance
+        {
+            get { return this._MinimumDistance; }
+            set { this._MinimumDistance = value; }
+        }
+
+        [JsonProperty("vol_scale")]
+        public float VolumeScale
+        {
+            get { return this._VolumeScale; }
+            set { this._VolumeScale = value; }
+        }
+
+        [JsonProperty("rolloff")]
+        public float Rolloff
+        {
+            get { return this._Rolloff; }
+            set { this._Rolloff = value; }
+        }
+
+        [JsonProperty("start_delay")]
+        public uint StartDelay
+        {
+            get { return this._StartDelay; }
+            set { this._StartDelay = value; }
+        }
+        #endregion
 
         public void Read(Stream input, uint version, Endian endian)
         {
-            var unknown0 = input.ReadValueU32(endian);
-            var unknown1 = Vector3.Read(input, endian);
-            var unknown2 = input.ReadValueB8();
-            var unknown3 = input.ReadStringU16(32, Encoding.ASCII, endian);
-            var unknown4 = input.ReadValueF32(endian);
-            var unknown5 = input.ReadValueF32(endian);
-            var unknown6 = input.ReadValueF32(endian);
-            var unknown7 = input.ReadValueU32(endian);
+            this._Uid = input.ReadValueU32(endian);
+            this._Position = Vector3.Read(input, endian);
+            this._PlayInEditor = input.ReadValueB8();
+            this._SoundFileName = input.ReadStringU16(32, Encoding.ASCII, endian);
+            this._MinimumDistance = input.ReadValueF32(endian);
+            this._VolumeScale = input.ReadValueF32(endian);
+            this._Rolloff = input.ReadValueF32(endian);
+            this._StartDelay = input.ReadValueU32(endian);
         }
 
         public void Write(Stream output, uint version, Endian endian)
         {
-            throw new NotImplementedException();
+            output.WriteValueU32(this._Uid, endian);
+            Vector3.Write(output, this._Position, endian);
+            output.WriteValueB8(this._PlayInEditor);
+            output.WriteStringU16(this._SoundFileName, 32, Encoding.ASCII, endian);
+            output.WriteValueF32(this._MinimumDistance, endian);
+            output.WriteValueF32(this._VolumeScale, endian);
+            output.WriteValueF32(this._Rolloff, endian);
+            output.WriteValueU32(this._StartDelay, endian);
+        }
+
+        public class ArrayElement : SerializableArrayElement<AmbientSoundElement>
+        {
         }
     }
 }
